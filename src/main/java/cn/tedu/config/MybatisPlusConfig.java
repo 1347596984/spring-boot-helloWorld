a57d590ef798785baccda1ec51946a015af53b9a
1347596984 <1347596984@qq.com>
2020-05-27 14:28:32 +0800

    修改hotfix问题

--- src/main/java/cn/tedu/config/MybatisPlusConfig.java
+++ src/main/java/cn/tedu/config/MybatisPlusConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("cn.tedu.dao*")//扫描文件
 public class MybatisPlusConfig {
-
+//紧急修改hotfix问题
     /**
      * 分页插件
      * @return
