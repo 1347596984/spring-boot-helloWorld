27b413fed8f8aebdf7c99ee7de79d4210c46f6ed
1347596984 <1347596984@qq.com>
2020-05-27 10:28:23 +0800

    第二次提交：提交至本地 zjp

--- src/main/java/cn/tedu/RunApp.java
+++ src/main/java/cn/tedu/RunApp.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 @ServletComponentScan
-@MapperScan("cn.tedu.ant.*.mapper")//启动类中添加这句，扫描dao文件
+@MapperScan("cn.tedu.ant.*.mapper")//启动类中添加这句，扫描dao文件       第二次提交测试
 //("com.baomidou.springboot.*.mapper")
 public class RunApp extends SpringBootServletInitializer {
     @Override
